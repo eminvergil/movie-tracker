1c37e7e6c86bd8a74dc660c7283f9cce7a977444
eminvergil <emintrhack@gmail.com>
2021-03-21 19:18:39 +0300

    added faker package to generate mock data, added necessary comps from react-native

--- App.js
+++ App.js
@@ -1,6 +1,29 @@
 import { StatusBar } from "expo-status-bar";
+
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Dimensions,
+  FlatList,
+  Image,
+} from "react-native";
+
+import faker from "faker";
+
+faker.seed(10);
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+const DATA2 = [...Array(30).keys()].map((_, i) => {
+  return {
+    key: faker.random.uuid(),
+    name: faker.first_name(),
+  };
+});
 
 export default function App() {
   return (
