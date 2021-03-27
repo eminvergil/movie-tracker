c22103645bdcc91630bbacbb8b8362be5f65f004
eminvergil <emintrhack@gmail.com>
2021-03-27 11:08:14 +0300

    test succesful

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 
-import React , {useState,useEffect}from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   Text,
-  View,Button,
+  View,
+  Button,
   TextInput,
   Dimensions,
   FlatList,
@@ -18,8 +19,10 @@ faker.seed(10);
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
+const ARRAY_LENG = 10;
+
 // key , name
-const DATA = [...Array(30).keys()].map((_, i) => {
+const DATA = [...Array(ARRAY_LENG).keys()].map((_, i) => {
   return {
     key: faker.random.uuid(),
     name: faker.name.firstName(),
@@ -27,40 +30,92 @@ const DATA = [...Array(30).keys()].map((_, i) => {
 });
 
 export default function App() {
-  const [clicked,setClicked] = useState({key:"",click: false});
+  const [clicked, setClicked] = useState([{ key: "", click: "", name: "" }]);
+
+  useEffect(() => {
+    for (let i = 0; i < ARRAY_LENG; i++) {
+      clicked[i] = { key: DATA[i].key, click: false, name: DATA[i].name };
+    }
+
+    // for (let i = 0; i < 30; i++) {
+    //   console.log(clicked[i]);
+
+    //   console.log(DATA[i]);
+    // }
+
+    console.log(clicked);
+
+    // DATA.forEach((item, index) => {
+    //   setClicked([{ key: item.key, click: false, name: item.name }]);
+    // });
+  }, []);
 
   return (
-    <View style={[styles.container,{marginTop:32}]}>
-
-      <Text style={{fontSize:24,justifySelf:"center"}}>Movie Tracker App</Text>
+    <View style={[styles.container, { marginTop: 32 }]}>
+      <Text style={{ fontSize: 24, justifySelf: "center" }}>
+        Movie Tracker App
+      </Text>
 
       <View style={styles.movielist}>
-        <FlatList data={DATA}
-                  // horizontal
-                  showHorizontalScrollIndicator={false}
-                  contentContainerStyle={{   }}
-                  keyExtractor={(item) => item.key}
-                  renderItem={({item}) => {
-                    return (
-                        <View
-                            key={item.key}
-                            style={[styles.filmitem,{
-                              backgroundColor: clicked.click ? "lightblue" : "green"}]}
-                        >
-                      <Text style={{fontSize:16,fontWeight:"bold",marginRight:"auto"}}>{item.name}</Text>
-                      <Button
-                          style={{marginRight: "auto" ,height:48, width:64 , alignSelf: "end"}}
-                          title="Press me"
-                          onPress={() => {
-                            setClicked({key: item.key, click: !clicked.click});
-                            console.log(clicked);
-                          }}
-                      />
-                    </View>
-                    );
+        <FlatList
+          data={clicked}
+          // horizontal
+          showHorizontalScrollIndicator={false}
+          contentContainerStyle={{}}
+          keyExtractor={(item) => item.key}
+          renderItem={({ item }) => {
+            return (
+              <View
+                key={item.key}
+                style={[
+                  styles.filmitem,
+                  {
+                    backgroundColor: item.click ? "lightblue" : "green",
+                  },
+                ]}
+              >
+                <Text
+                  style={{
+                    fontSize: 16,
+                    fontWeight: "bold",
+                    marginRight: "auto",
                   }}
-        />
+                >
+                  {item.name}
+                </Text>
+                <Button
+                  style={{
+                    marginRight: "auto",
+                    height: 48,
+                    width: 64,
+                    alignSelf: "end",
+                  }}
+                  title="Press me"
+                  onPress={() => {
+                    const _item = clicked.findIndex((it) => it.key == item.key);
+                    // .filter((x) => x.true);
 
+                    console.log(_item);
+
+                    let new_copy = [...clicked];
+
+                    new_copy[_item] = {
+                      ...new_copy[_item],
+                      click: !new_copy[_item].click,
+                    };
+
+                    setClicked(new_copy);
+
+                    // setClicked(items);
+                    // console.log(item.key);
+
+                    // setClicked(items);
+                  }}
+                />
+              </View>
+            );
+          }}
+        />
       </View>
     </View>
   );
@@ -75,17 +130,17 @@ const styles = StyleSheet.create({
     // alignItems: "center",
     // justifyContent: "center",
   },
-  movielist:{
+  movielist: {
     margin: 12,
     marginHorizontal: 16,
     // backgroundColor: "#cc35"
   },
-  filmitem:{
+  filmitem: {
     height: 150,
-    display:"flex",
+    display: "flex",
     flexDirection: "row",
     marginVertical: 4,
     padding: 10,
     borderRadius: 12,
-  }
+  },
 });
